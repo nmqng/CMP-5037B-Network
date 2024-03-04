14d1372ee0baaba97077eb0b26975ed002767a87
jamesngn <ngmqng@gmail.com>
2024-03-04 14:40:36 +0000

    update coursework structure

---
+++ datagramsocket2/Sender.java
@@ -0,0 +1,71 @@
+import CMPC3M06.AudioRecorder;
+
+import javax.sound.sampled.LineUnavailableException;
+import java.net.*;
+import java.nio.ByteBuffer;
+
+public class Sender {
+    static DatagramSocket sending_socket;
+    public static void main(String[] args) throws Exception{
+        {
+            int PORT = 55555;
+            InetAddress clientIP = null;
+            try {
+                clientIP = InetAddress.getByName("localhost");
+            } catch (UnknownHostException e) {
+                System.out.println("ERROR: AudioSender: Could not find client IP");
+                e.printStackTrace();
+                System.exit(0);
+            }
+
+            try {
+                sending_socket = new DatagramSocket();
+            } catch (SocketException e) {
+                System.out.println("ERROR: AudioSender: Could not open UDP socket to send from.");
+                e.printStackTrace();
+                System.exit(0);
+            }
+
+            AudioRecorder recorder = null;
+            try {
+                recorder = new AudioRecorder();
+            } catch (LineUnavailableException e) {
+                throw new RuntimeException(e);
+            }
+            int recordTime = 10;
+            int key = 15;
+            // Main loop
+            boolean running = true;
+            while (running) {
+                try {
+                    System.out.println("Recording Audio...");
+                    for (int i = 0; i < Math.ceil(recordTime / 0.032); i++) {
+                        byte[] block = recorder.getBlock();
+                        short authenticationKey = 10;
+                        // encrypt data from here
+                        ByteBuffer unwrapEncrypt = ByteBuffer.allocate(block.length);
+                        ByteBuffer plainText = ByteBuffer.wrap(block);
+                        for( int j = 0; j < block.length/4; j++) {
+                            int fourByte = plainText.getInt();
+                            fourByte = fourByte ^ key; // XOR operation with key
+                            unwrapEncrypt.putInt(fourByte);
+                        }
+                        byte[] encryptedBlock = unwrapEncrypt.array();
+                        // This line authentication (payload with header) VoIPpacket
+                        ByteBuffer VoIPpacket = ByteBuffer.allocate(encryptedBlock.length + 2);
+                        // put the block into the VoIPpacket
+                        VoIPpacket.put(encryptedBlock);
+                        VoIPpacket.putShort(authenticationKey);
+                        DatagramPacket packet = new DatagramPacket(VoIPpacket.array(), VoIPpacket.array().length, clientIP, PORT);
+                        sending_socket.send(packet);
+                    }
+                    running = false;
+                } catch (Exception e) {
+                    System.out.println("ERROR: AudioSender: Some error occurred!");
+                    e.printStackTrace();
+                }
+            }
+            sending_socket.close();
+        }
+    }
+}
---
+++ datagramsocket4/Receiver.java
@@ -0,0 +1,58 @@
+import CMPC3M06.AudioPlayer;
+
+import javax.sound.sampled.LineUnavailableException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+
+public class Receiver {
+    static DatagramSocket receiving_socket;
+    public static void main(String[] args) {
+        {
+            int PORT = 55555;
+            try {
+                receiving_socket = new DatagramSocket(PORT);
+            } catch (SocketException e){
+                System.out.println("ERROR: AudioReceiver: Could not open UDP socket to receive from.");
+                e.printStackTrace();
+                System.exit(0);
+            }
+            boolean running = true;
+            byte[] buffer = new byte[514];
+            int key = 15;
+            DatagramPacket packet = new DatagramPacket(buffer, 0, buffer.length);
+            AudioPlayer player = null;
+            try {
+                player = new AudioPlayer();
+            } catch (LineUnavailableException e) {
+                throw new RuntimeException(e);
+            }
+            while (running){
+                try{
+                    receiving_socket.receive(packet);
+                    // decrypt data from here
+                    ByteBuffer unwrapDecrypt = ByteBuffer.allocate(packet.getLength()); // allocate block length
+                    ByteBuffer cipherText = ByteBuffer.wrap(packet.getData()); // wrap the encryptedBlock
+                    short authenticationKey = cipherText.getShort();
+                    if (authenticationKey == 10){
+                        for(int j = 0; j < packet.getLength()/4; j++) {
+                            int fourByte = cipherText.getInt();
+                            fourByte = fourByte ^ key; // XOR decrypt
+                            unwrapDecrypt.putInt(fourByte);
+                        }
+                        byte[] decryptedBlock = unwrapDecrypt.array();
+                        player.playBlock(decryptedBlock);
+                    } else {
+                        System.out.println("Authentication key not match");
+                    }
+                } catch (Exception e){
+                    System.out.println("ERROR: AudioReceiver: Some error occurred!");
+                    e.printStackTrace();
+                }
+            }
+            player.close();
+            receiving_socket.close();
+        }
+    }
+}
